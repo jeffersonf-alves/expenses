b1da9f65e0ea3d88a9eac916b001d7b66c098cb5
Jefferson Alves <61715554+jeffersonf-alves@users.noreply.github.com>
2025-02-13 21:35:55 -0300

    Importando intl

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
-
+import 'package:intl/intl.dart';
 
 void main(List<String> args) {
     runApp(ExpensesApp());
@@ -107,7 +107,7 @@ class MyHomePage extends StatelessWidget {
                                   )  
                                 ),
                               Text(
-                                tr.date.toString(),
+                                DateFormat('dMMMy').format(tr.date),
                                 style: TextStyle(
                                   color: Colors.grey[600]
                                 ),
