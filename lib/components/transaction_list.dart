1c21c05b89c97b32bc2fd8b97e808155fdd34dd4
Jefferson Alves <61715554+jeffersonf-alves@users.noreply.github.com>
2025-02-21 23:34:47 -0300

    Separando widgets

---
+++ lib/components/transaction_list.dart
@@ -0,0 +1,14 @@
+import 'package:expenses/models/transaction.dart';
+import 'package:flutter/material.dart';
+
+class TransactionList extends StatelessWidget {
+  final List<Transaction> transactions;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+      return Container(
+
+      );
+  }
+}
