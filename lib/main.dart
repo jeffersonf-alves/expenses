c61eb0799a4535e3ddc246222ee6b3d533cd33f8
Jefferson Alves <61715554+jeffersonf-alves@users.noreply.github.com>
2025-02-22 18:54:22 -0300

    Correção

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+
 
 void main(List<String> args) {
     runApp(ExpensesApp());
@@ -77,58 +77,6 @@ class MyHomePage extends StatelessWidget {
                     child: Text('Gráfico')
                 )
               ),
-              Column(
-                children: _transactions.map((tr) {
-                  return Card(
-                      child: Row(
-                        children: [
-                          Container(
-                            margin: EdgeInsets.symmetric(
-                              horizontal: 15,
-                              vertical: 10
-                            ),
-                            decoration: BoxDecoration(
-                              border: Border.all(
-                                color: Colors.purple,
-                                width: 2,
-
-                              )
-                            ),
-                            padding: EdgeInsets.all(10),
-                            child: Text(
-                              'R\$ ${tr.value.toStringAsFixed(2)}',
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 20,
-                                color: Colors.purple,
-
-                              )
-                            ),
-                          ),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                tr.title,
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold,
-
-                                  )  
-                                ),
-                              Text(
-                                DateFormat('d MMM y').format(tr.date),
-                                style: TextStyle(
-                                  color: Colors.grey[600]
-                                ),
-                                )
-                            ],
-                          )
-                        ],
-                      ),
-                  );
-                }).toList(),
-              ),
               Card(
                 elevation: 5,
                 child: Container(
@@ -149,19 +97,21 @@ class MyHomePage extends StatelessWidget {
                       mainAxisAlignment: MainAxisAlignment.end,
                       children: [
                         FloatingActionButton(
-                            child: Text('Nova transação'),
-                            textColor: Colors.purple,
-                            onPressed: () {
-                              print(titleController.text);
-                              print(valueController.text);
-                            },
+                          backgroundColor: Colors.purple, // Define a cor de fundo
+                          onPressed: () {
+                            print(titleController.text);
+                            print(valueController.text);
+                          },
+                          child: Text(
+                            'Nova transação',
+                            style: TextStyle(color: Colors.white), // Define a cor do texto
+                          ),
                         )
                       ],
                     )
                   ],
                 ),
                 )
-              
               )
           ],
         )
